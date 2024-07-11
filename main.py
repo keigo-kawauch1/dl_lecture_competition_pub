f901c954f17b1f0fd2bbc1c8ec98e956d83dd436
ist-ipc3_gpu <ipc3.gpu@gmail.com>
2024-07-11 08:59:16 +0000

    chore:.toml

--- main.py
+++ main.py
@@ -363,6 +363,7 @@ def main():
     set_seed(42)
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    print(device)
     # dataloader / model
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -371,10 +372,10 @@ def main():
     train_dataset = VQADataset(df_path="./data/train.json", image_dir="./data/train", transform=transform)
     test_dataset = VQADataset(df_path="./data/valid.json", image_dir="./data/valid", transform=transform, answer=False)
     test_dataset.update_dict(train_dataset)
-
+    
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
-
+    
     model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device)
 
     # optimizer / criterion
